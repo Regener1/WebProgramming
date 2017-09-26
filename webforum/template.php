e7001ab880dec9d25d3d84770060b941ea8f85a0
Regener1 <st.8.sh@mail.ru>
2017-09-26 18:32:30 +0300

    empty html

---
+++ webforum/template.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+
+    </title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="Refresh" content="2" />
+</head>
+
+<body>
+    <div id="wrap-nav">
+        <div class="site-nav">
+            <div class="nav-menu dropdown hidden">
+                <ul class="dropdown-content">
+                    <li
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div id="wrap-header">
+
+    </div>
+
+    <div id="wrap-body">
+
+    </div>
+
+    <div id="wrap-footer">
+
+    </div>
+</body>
+</html>
