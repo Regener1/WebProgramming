11dd56f32a53ef3acb1bdfc074210e8dbea0c89b
Regener1 <st.8.sh@mail.ru>
2017-10-01 23:37:05 +0300

    test git

--- webforum/template.php
+++ webforum/template.php
@@ -6,30 +6,128 @@
     </title>
 
     <meta charset="utf-8">
-    <meta http-equiv="Refresh" content="2" />
+    <meta http-equiv="Refresh" content="0.01"/>
+
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/color.css" rel="stylesheet">
+
 </head>
 
 <body>
+
+
+<div id="wrap-head">
+
     <div id="wrap-nav">
-        <div class="site-nav">
-            <div class="nav-menu dropdown hidden">
-                <ul class="dropdown-content">
-                    <li
-                </ul>
-            </div>
+        <div class="chunk">
+            <ul class="navmenu inlinelist dropdown">
+                <!--Dropdown menu-->
+                <li class="dropdown-container">Quick links
+                    <ul class="dropdown-content">
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Our rules</a></li>
+                        <li><a href="#">szfvzsdf</a></li>
+                    </ul>
+                </li>
+                <!--Register-->
+                <li class="rightside">
+                    <a href="#">Register</a>
+                </li>
+
+            </ul>
+
         </div>
     </div>
 
     <div id="wrap-header">
-
+        <div class="site-header light-gray">
+            <div class="site-logo"><a href="#"></a></div>
+            <ul class="site-menu inlinelist">
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Our rules</a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </div>
     </div>
+</div>
 
-    <div id="wrap-body">
+<div id="wrap-body">
+    <div class="chunk">
+        <div id="sidebar">
+            <div class="forumblock dark">
+                <div class="header">Last posts</div>
+                <ul class="forums inlinelist">
+                    <li class="rowcontent row">
+                        Re: <a href="#">Post Sample</a><br>by
+                        <a href="#">User123</a> 07 Dec 2015, 20:20
 
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div id="forumlist">
+            <div class="forumblock blue">
+                <div class="header">Theme</div>
+                <ul class="forums">
+                    <li class="row">
+                        <ul class="rowcontent inlinelist">
+                            <li class="theme"><a href="#">Information</a></li>
+                            <li class="posts">3 topics<br>2 posts</li>
+                            <li class="lastpost">Re: <a href="#">Post Sample</a><br>by
+                                <a href="#">User123</a> 07 Dec 2015, 20:20
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="row">
+                        <ul class="rowcontent inlinelist">
+                            <li class="theme"><a href="#">Information</a></li>
+                            <li class="posts">3 topics<br>2 posts</li>
+                            <li class="lastpost">Re: <a href="#">Post Sample</a><br>by
+                                <a href="#">User123</a> 07 Dec 2015, 20:20
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="row">
+                        <ul class="rowcontent inlinelist">
+                            <li class="theme"><a href="#">Information</a></li>
+                            <li class="posts">3 topics<br>2 posts</li>
+                            <li class="lastpost">Re: <a href="#">Post Sample</a><br>by
+                                <a href="#">User123</a> 07 Dec 2015, 20:20
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
+</div>
 
-    <div id="wrap-footer">
+<div id="wrap-footer">
+    <div class="chunk">
+        <div class="inner">
+            <div class="footerblock">
+                <h5>About us</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin
+                    gravida
+                    dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis
+                    natoq</p>
+            </div>
 
+            <div class="footerblock">
+                <h5>You Must Know</h5>
+                <ul>
+                    <li><a href="#">Our rules</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            </div>
+
+            <div class="footerblock">
+                <h5>Contact us</h5>
+                <p>Admin e-mail: something@mail.ru</p>
+            </div>
+        </div>
     </div>
+</div>
 </body>
 </html>
