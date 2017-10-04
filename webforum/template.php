06f4c7581df51424e9b73632ab6a1f164c294d64
Regener1 <st.8.sh@mail.ru>
2017-10-04 21:50:37 +0300

    work on adaptivity

--- webforum/template.php
+++ webforum/template.php
@@ -6,7 +6,7 @@
     </title>
 
     <meta charset="utf-8">
-<!--    <meta http-equiv="Refresh" content="0.01"/>-->
+    <!--    <meta http-equiv="Refresh" content="0.01"/>-->
 
     <link href="css/style.css" rel="stylesheet">
     <link href="css/color.css" rel="stylesheet">
@@ -85,37 +85,39 @@
         </div>
 
         <div id="forumlist">
-            <div class="forumblock blue">
-                <div class="header">Theme</div>
-                <ul class="forums">
-                    <li class="row">
-                        <ul class="rowcontent inlinelist">
-                            <li class="theme"><a href="#">Information</a></li>
-                            <li class="posts">3 topics<br>2 posts</li>
-                            <li class="lastpost">Re: <a href="#">Post Sample</a><br>by
-                                <a href="#">User123</a> 07 Dec 2015, 20:20
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="row">
-                        <ul class="rowcontent inlinelist">
-                            <li class="theme"><a href="#">Information</a></li>
-                            <li class="posts">3 topics<br>2 posts</li>
-                            <li class="lastpost">Re: <a href="#">Post Sample</a><br>by
-                                <a href="#">User123</a> 07 Dec 2015, 20:20
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="row">
-                        <ul class="rowcontent inlinelist">
-                            <li class="theme"><a href="#">Information</a></li>
-                            <li class="posts">3 topics<br>2 posts</li>
-                            <li class="lastpost">Re: <a href="#">Post Sample</a><br>by
-                                <a href="#">User123</a> 07 Dec 2015, 20:20
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+            <div id="forumlist-inner">
+                <div class="forumblock blue">
+                    <div class="header">Theme</div>
+                    <ul class="forums">
+                        <li class="row">
+                            <ul class="rowcontent inlinelist">
+                                <li class="theme"><a href="#">Information</a></li>
+                                <li class="posts">3 topics<br>2 posts</li>
+                                <li class="lastpost">Re: <a href="#">Post Sample</a><br>by
+                                    <a href="#">User123</a> 07 Dec 2015, 20:20
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="row">
+                            <ul class="rowcontent inlinelist">
+                                <li class="theme"><a href="#">Information</a></li>
+                                <li class="posts">3 topics<br>2 posts</li>
+                                <li class="lastpost">Re: <a href="#">Post Sample</a><br>by
+                                    <a href="#">User123</a> 07 Dec 2015, 20:20
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="row">
+                            <ul class="rowcontent inlinelist">
+                                <li class="theme"><a href="#">Information</a></li>
+                                <li class="posts">3 topics<br>2 posts</li>
+                                <li class="lastpost">Re: <a href="#">Post Sample</a><br>by
+                                    <a href="#">User123</a> 07 Dec 2015, 20:20
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -148,6 +150,6 @@
     </div>
 </div>
 
-<script type="text/javascript" src="js/js_script.js" ></script>
+<script type="text/javascript" src="js/js_script.js"></script>
 </body>
 </html>
